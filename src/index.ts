72e8f9200d57831f77cea3c2494ee5e5a3a67a7b
rugamoto <apetoshi.rugamoto@gmail.com>
2022-06-13 23:52:33 +0100

    exports, typedoc

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,6 @@
 export { Socket } from "./socket";
+export { SocketTx } from "./socketTx";
+export { Path } from "./path";
+export { TokenAsset } from "./client/models/TokenAsset";
+export * from "./types";
+export * from "./client";
