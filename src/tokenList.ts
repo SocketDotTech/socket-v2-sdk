f3370689b855125ca1e39302bbffa7ced36a54a9
rugamoto <apetoshi.rugamoto@gmail.com>
2022-06-29 16:52:53 +0100

    fix token list array

--- src/tokenList.ts
+++ src/tokenList.ts
@@ -2,23 +2,24 @@ import { ChainId } from "@socket.tech/ll-core/constants/types";
 import { Token } from ".";
 import { NATIVE_TOKEN_ADDRESS } from "./constants";
 
-export class TokenList extends Array<Token> {
+export class TokenList {
+  tokens: Array<Token>;
   chainId: ChainId;
 
-  constructor(chainId: ChainId, items: Array<Token>) {
-    super(...items);
+  constructor(chainId: ChainId, tokens: Array<Token>) {
+    this.tokens = tokens
     this.chainId = chainId;
   }
 
   tokenByAddress(address: string) {
-    return this.find((token) => token.address.toLowerCase() === address.toLowerCase());
+    return this.tokens.find((token) => token.address.toLowerCase() === address.toLowerCase());
   }
 
   tokenBySymbol(symbol: string) {
-    return this.find((token) => token.symbol === symbol);
+    return this.tokens.find((token) => token.symbol === symbol);
   }
 
   nativeToken() {
-    return this.find((token) => token.address.toLowerCase() === NATIVE_TOKEN_ADDRESS);
+    return this.tokens.find((token) => token.address.toLowerCase() === NATIVE_TOKEN_ADDRESS);
   }
 }
