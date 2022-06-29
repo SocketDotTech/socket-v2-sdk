6ff5913b4ded6b370957794e1df0a3dcdae62864
rugamoto <apetoshi.rugamoto@gmail.com>
2022-06-29 16:13:45 +0100

    more export

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,8 @@ export { ConnectedSocket } from "./connectedSocket";
 export { SocketTx } from "./socketTx";
 export { Path } from "./path";
 export { Token } from "./client/models/Token";
+export { TokenList } from "./tokenList"
+export { Chain } from "./chain"
 export * as constants from "./constants";
 export * from "./types";
 export * from "./client";
